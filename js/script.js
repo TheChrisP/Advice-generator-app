7a8cb4bb9c0b09039c6f163eaf15a06b3729132d
TheChrisP <christoph.pabel@gmail.com>
2022-03-27 19:02:37 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// const submit = document.getElementById('submit');
+const randomBtn = document.getElementById('random-btn');
+const quote = document.getElementById('quote');
+const adviceNumber = document.getElementById('advice-number');
+
+function getRandomAdvice(e) {
+  e.preventDefault();
+
+  fetch(`https://api.adviceslip.com/advice`)
+    .then((res) => res.json())
+    .then((data) => {
+      adviceNumber.innerHTML = `Advice #${data.slip.id}`;
+      quote.innerHTML = `"${data.slip.advice}"`;
+    });
+}
+
+randomBtn.addEventListener('click', getRandomAdvice);
